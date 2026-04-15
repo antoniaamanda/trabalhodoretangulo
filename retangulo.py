7d0e99f991ee9ca5c36afba307536cbf1812ec32
antoniaamanda <caped.2025123isinf0010@aluno.ifpi.edu.br>
2026-04-15 14:56:25 -0300

    primeiro commit

---
+++ retangulo.py
@@ -0,0 +1,8 @@
+#Programa para calcular a área de um retângulo
+
+base = float(input("digite o valor da base: "))
+altura = float(input("digite o valor da altura: "))
+
+area = base * altura
+
+print(f"A área do retângulo é: {area}")
